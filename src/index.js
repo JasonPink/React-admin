30a5238c74dbf58b2363368c348f22714f53315e
JasonPink <lee_yemeng@163.com>
2020-09-07 23:10:14 +0800

    配置less

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import SiderDemo from './components/SiderDemo'
+import './style/index.less'
 
 ReactDOM.render(
   <React.StrictMode>
